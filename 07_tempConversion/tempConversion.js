1810d8f244412fa15cf74687d7bcebf1814fae89
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-10 16:24:44 -0500

    Compl;ete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const calculatedTemp = (fahrenheit - 32) * (5 / 9);
+  return Number.isInteger(calculatedTemp) ? calculatedTemp : Number(calculatedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const calculatedTemp = celsius * (9 / 5) + 32;
+  return Number.isInteger(calculatedTemp) ? calculatedTemp : Number(calculatedTemp.toFixed(1));
 };
 
 // Do not edit below this line
