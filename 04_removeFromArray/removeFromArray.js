c3e070197a6d0fae5b632b2f1f67f5b1e78b38f2
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-10 15:44:12 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+function removeArrayElement(array, num) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === num) {
+            while (array[i] === num) {
+                array.splice(i, 1);
+            }
+        }
+    }
+
+    return array
+}
+
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        array = removeArrayElement(array, args[i]);
+    }
+
+    return array
 };
 
 // Do not edit below this line
