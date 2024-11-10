25c687f84c7c4dd5338b4c4034cb48ff353164ac
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-10 15:57:37 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    if (Math.floor(num1) != num1 || Math.floor(num2) != num2) {
+        return 'ERROR';
+    }
+
+    if (typeof num1 != typeof 1 || typeof num2 != typeof 1) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    let start = 0;
+    let end = 0;
+    
+    if (num1 <= num2) {
+        start = num1;
+        end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
